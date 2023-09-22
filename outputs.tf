a56205811f5546582aea9351803f26f406cc5bd5
Jerome Wolff <Ic3w0lf@users.noreply.github.com>
2023-09-22 10:14:28 +0200

    Make customer KMS encryption optional

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "name" {
   description = "The cloudwatch log group name"
   value       = aws_cloudwatch_log_group.main.name
 }
+
+output "customer_managed_key_arn" {
+  description = "The ARN of the customer KMS key used to encrypt log data if enabled."
+  value       = var.enable_customer_managed_kms ? module.kms[0].key_arn : null
+}
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "tags" {
   type        = map(any)
 }
 
-# AWS Cloudwatch log groups
+# AWS Cloudwatch Log Group
 variable "name" {
   description = "The name of the log group."
   type        = string
@@ -19,6 +19,26 @@ variable "retention_in_days" {
 
 variable "skip_destroy" {
   description = "Set to true if you do not wish the log group (and any logs it may contain) to be deleted at destroy time, and instead just remove the log group from the Terraform state."
+  default     = false
+  type        = bool
+}
+
+# Log Streams
+variable "log_streams" {
+  description = "A list of log streams to create within the log group."
+  default     = []
+  type        = list(string)
+}
+
+# KMS Encryption
+variable "kms_key_id" {
+  description = "The ARN of the KMS Key to use when encrypting log data."
+  default     = null
+  type        = string
+}
+
+variable "enable_customer_managed_kms" {
+  description = "Whether to enable customer managed KMS encryption for the log group."
   default     = false
   type        = bool
 }
